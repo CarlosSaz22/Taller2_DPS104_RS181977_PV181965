6ab592ea82a03fee427d1b19ef6b862d72963b7d
CarlosSaz22 <61812538+CarlosSaz22@users.noreply.github.com>
2021-10-08 16:30:37 -0600

    Se agrega componente resultado y se agrega funcionabilidad al componente resta

---
+++ taller2/src/Pantallas/Resultado.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function Result(props) {
+  const { total, errorMessage } = props;
+  return (
+    <View style={styles.content}>
+      {total && (
+        <View style={styles.boxResult}>
+          <Text style={styles.title}>Resultado : {total}</Text>
+        </View>
+      )}
+      <View>
+        <Text style={styles.error}>{errorMessage}</Text>
+      </View>
+    </View>
+  );
+}
+function DataResult(props) {
+  const { title, value } = props;
+  return (
+    <View style={styles.value}>
+      <Text>{title}</Text>
+      <Text>{value}</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  content: {
+    position: 'relative',
+    marginTop: 250,
+    marginHorizontal: 40,
+  },
+  boxResult: {
+    padding: 30,
+  },
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  value: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+});
